6e08c6a54107f5b78650e70c87066755a57dbeb3
ADMIN <dishajatkar@gmail.com>
2026-02-27 13:30:09 +0530

    Cookies in Route handler

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,6 +1,6 @@
 // Readonly headers
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 // export async function GET() {
 //   return new Response("Profile API data!");
@@ -12,9 +12,21 @@ export async function GET(request: NextRequest) {
 
   const headerList = await headers();
   console.log("Headers from next/headers:", headerList.get("Authorization"));
+
+  const theme = request.cookies.get("theme");
+  console.log("Theme cookie value:", theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("resultPerPage", "10");
+  console.log(
+    "Cookies from next/cookies:",
+    cookieStore.get("resultPerPage"),
+  );
+
   return new Response("<h1>Profile API data!</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "set-cookie": "theme=dark",
     },
   });
 }
