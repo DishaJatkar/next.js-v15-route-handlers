112c74cc202ccf3caab07d186e430981c92b6c9d
ADMIN <dishajatkar@gmail.com>
2026-02-27 10:40:36 +0530

    Headers in Route handler

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,20 @@
-export async function GET() {
-  return new Response("Profile API data!");
+// Readonly headers
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+// export async function GET() {
+//   return new Response("Profile API data!");
+// }
+
+export async function GET(request: NextRequest) {
+  // const requestHeaders = new Headers(request.headers);
+  // console.log("Request Headers:", requestHeaders.get("Authorization"));
+
+  const headerList = await headers();
+  console.log("Headers from next/headers:", headerList.get("Authorization"));
+  return new Response("<h1>Profile API data!</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
